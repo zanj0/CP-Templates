1b3fe4f8cedb07c72db274576f9f6842b6660195
Agnibha Chakraborty <agnibhachakraborty@gmail.com>
2026-02-01 07:04:43 +0530

    Add lli version for CHT

--- Monotonic Convex Hull 1.cpp
+++ Monotonic Convex Hull 1.cpp
@@ -50,3 +50,65 @@ struct CHT {
         return qry.first.val(x);
     }
 };
+
+**************
+
+struct CHT
+{
+    struct Line
+    {
+        lli slope, yIntercept;
+        Line(lli slope, lli yIntercept)
+        {
+            this->slope = slope;
+            this->yIntercept = yIntercept;
+        }
+
+        lli GetValue(lli x)
+        {
+            return slope * x + yIntercept;
+        }
+        lli CeilDiv(lli a, lli b)
+        {
+            return (a + b - 1) / b;
+        }
+        lli GetIntersection(Line &other)
+        {
+            // y1 = m1x + c
+            // y2 = m2x + c2
+            // m1x + c1 = m2x + c2
+            // x = (c2 - c1) / (m1 - m2)
+
+            return CeilDiv((other.yIntercept - yIntercept), (slope - other.slope));
+        }
+    };
+    deque<pair<Line, lli>> dq;
+    void InsertLine(Line new_line)
+    {
+        while (!dq.empty() && dq.back().ss >= dq.back().ff.GetIntersection(new_line))
+        {
+            dq.pop_back();
+        }
+
+        if (dq.empty())
+        {
+            dq.pb({new_line, 0});
+        }
+        else
+        {
+            dq.pb({new_line, dq.back().ff.GetIntersection(new_line)});
+        }
+    }
+
+    lli Query(lli x)
+    {
+        while (dq.size() > 1)
+        {
+            if (dq[1].ss <= x)
+                dq.pop_front();
+            else
+                break;
+        }
+        return dq[0].ff.GetValue(x);
+    }
+};
